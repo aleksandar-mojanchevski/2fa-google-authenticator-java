7bdb8c37365a6877580363451ef3c6826abf9b39
alex <aleksandar.mojanchevski@atis.al>
2024-07-26 04:45:38 +0200

    secret uppercase

--- src/main/java/com/example/auth/google/MainApplication.java
+++ src/main/java/com/example/auth/google/MainApplication.java
@@ -3,10 +3,11 @@ package com.example.auth.google;
 public class MainApplication {
 
 	public static void main(String[] args) throws Exception {
-		String gitSecretKey = "ENTERSECRETUPPERCASE";
-		String googleSecretKey = "ENTERSECRETUPPERCASE";
+		String gitSecretKey = "O65RGRM5UYRGFC44";
+		String googleSecretKey = "IHXK GAO3 4ZG7 DXDE 3BCP 5AT4 6PTU GRZ3";
+		String hypercubegoogleKey = "JDRA YTJ6 FCMW DHO3 DFV3 IZXT O3QC DEEK";
 
-		String secretKey = googleSecretKey;
+		String secretKey = hypercubegoogleKey;
 		String code = Utils.getTOTPCode(secretKey);
 		System.out.println(code);
 		while (true) {
