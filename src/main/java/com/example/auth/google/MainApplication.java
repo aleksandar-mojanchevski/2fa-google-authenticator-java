4344184cb2f448d30add002599bd0369ce7c5094
aleksandar-mojanchevski <75128303+aleksandar-mojanchevski@users.noreply.github.com>
2024-12-17 19:34:56 +0100

    Update MainApplication.java

--- src/main/java/com/example/auth/google/MainApplication.java
+++ src/main/java/com/example/auth/google/MainApplication.java
@@ -3,9 +3,9 @@ package com.example.auth.google;
 public class MainApplication {
 
 	public static void main(String[] args) throws Exception {
-		String gitSecretKey = "O65RGRM5UYRGFC44";
-		String googleSecretKey = "IHXK GAO3 4ZG7 DXDE 3BCP 5AT4 6PTU GRZ3";
-		String hypercubegoogleKey = "JDRA YTJ6 FCMW DHO3 DFV3 IZXT O3QC DEEK";
+		String gitSecretKey = "setsecret";
+		String googleSecretKey = "setsecret";
+		String hypercubegoogleKey = "setsecret";
 
 		String secretKey = hypercubegoogleKey;
 		String code = Utils.getTOTPCode(secretKey);
